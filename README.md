f96b673bf61043e2de63aab891e3421ed8d66795
Andrey Masterenko <masterenko.as@gmail.com>
2021-05-19 21:42:59 +0300

    1. Загрузка данных в вид [#295782]

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Car accident
+[![Build Status](https://travis-ci.com/amasterenko/job4j_car_accident.svg?branch=main)](https://travis-ci.com/amasterenko/job4j_car_accident)  
+
 ____ 
--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -3,11 +3,20 @@ package ru.job4j.accident.control;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 
 @Controller
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        List<String> items = List.of(
+                "description1",
+                "description2",
+                "description3",
+                "description4",
+                "description5"
+        );
+        model.addAttribute("items", items);
         return "index";
     }
 }
