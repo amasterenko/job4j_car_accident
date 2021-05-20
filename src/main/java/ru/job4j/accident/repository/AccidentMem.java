a83d32abf465143a62ce097bd8352893a2e29c1f
Andrey Masterenko <masterenko.as@gmail.com>
2021-05-20 17:46:39 +0300

    3. @ModelAttribute. Создание инцидента. [#261013] - edited: AccidentMem indx field type was changed

--- src/main/java/ru/job4j/accident/repository/AccidentMem.java
+++ src/main/java/ru/job4j/accident/repository/AccidentMem.java
@@ -6,14 +6,16 @@ import ru.job4j.accident.model.Accident;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class AccidentMem {
-    private Map<Integer, Accident> accidents;
-    private int indx = 5;
+    private final Map<Integer, Accident> accidents;
+    private final AtomicInteger indx = new AtomicInteger(5);
 
     public AccidentMem() {
-        this.accidents = new HashMap<>() {{
+        this.accidents = new ConcurrentHashMap<>() {{
             put(0, new Accident(0, "accident1", "accident1 text", "address1"));
             put(1, new Accident(1, "accident2", "accident2 text", "address2"));
             put(2, new Accident(2, "accident3", "accident3 text", "address3"));
@@ -27,7 +29,7 @@ public class AccidentMem {
     }
 
     public Accident create(Accident accident) {
-        accident.setId(indx++);
+        accident.setId(indx.getAndIncrement());
         this.accidents.put(accident.getId(), accident);
         return accident;
     }
